75fa2a17fa1448d8074fc329e54cec419f860b36
Oliver (T560) <olli.bienert@gmail.com>
2019-10-16 15:59:31 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dj_montedb',
+    version='0.1',
+    packages=[''],
+    url='http://montehelfer.de',
+    license='MIT',
+    author='Oliver Bienert',
+    author_email='oliver.bienert@gmx.de',
+    description='Schul- und Kindergartenverwaltung'
+)
