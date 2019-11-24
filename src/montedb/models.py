e980c5e0387e2503ced79d2d2b6681fabb1f150d
Adrian Bielefeldt <martenbochum@gmx.de>
2019-11-24 01:24:34 +0100

    FIX corrected models to actually work

--- src/montedb/models.py
+++ src/montedb/models.py
@@ -1,10 +1,12 @@
+import datetime
+
 from django.db import models
 
 
 class Person(models.Model):
     last_name = models.CharField(max_length=255)
     first_name = models.CharField(max_length=255)
-    birth_date = models.DateField
+    birth_date = models.DateField(default=datetime.date.today)
 
     def __str__(self):
         return "Person (First name: {}, Last name: {}, Birth date: {}".format(
@@ -16,5 +18,21 @@ class Person(models.Model):
 
 class Child(Person):
     birth_place = models.CharField(max_length=255)
-    care_time = models.DurationField
-    kita = models.BooleanField
+    care_time = models.DurationField(default=datetime.timedelta(hours=1))
+    kita = models.BooleanField()
+
+    def __str__(self):
+        return "Child (" \
+               "First name: {}, " \
+               "Last name: {}, " \
+               "Birth date: {}, " \
+               "Birth place: {}, " \
+               "Care time:  {}, " \
+               "Kita: {}".format(
+            self.first_name,
+            self.last_name,
+            self.birth_date,
+            self.birth_place,
+            self.care_time,
+            self.kita)
+
