bf00808004c29d611165788a5371a8309393db4b
Adrian Bielefeldt <martenbochum@gmx.de>
2019-11-24 01:02:08 +0100

    Activated montedb app

--- src/montedb/models.py
+++ src/montedb/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    last_name = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    birth_date = models.DateField
+
+    def __str__(self):
+        return "Person (First name: {}, Last name: {}, Birth date: {}".format(
+            self.first_name,
+            self.last_name,
+            self.birth_date
+        )
+
+
+class Child(Person):
+    birth_place = models.CharField(max_length=255)
+    care_time = models.DurationField
+    kita = models.BooleanField
