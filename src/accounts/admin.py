79ff885cc5beaaf9899572d3c9f0f055748a54eb
Oliver (T560) <olli.bienert@gmail.com>
2020-01-21 16:39:17 +0100

    Fixed broken password change link in admin

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.forms import UserChangeForm
+from authtools.admin import UserAdmin as AuthtoolsUserAdmin
+from authtools.models import User
 
-# Register your models here.
+# HACK: In user admin detail view, the change password link
+# doesn't work. This hack fixes the issue until a fix is
+# release.
+# https://github.com/fusionbox/django-authtools/issues/91
+admin.site.unregister(User)
+
+
+@admin.register(User)
+class UserAdmin(AuthtoolsUserAdmin):
+    form = UserChangeForm
