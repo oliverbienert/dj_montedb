caefd11788a3384e3cc48bac1599e8c18dffe119
Oliver (T560) <olli.bienert@gmail.com>
2020-02-25 23:45:08 +0100

    Js calendar, year dropdown: show last 100 years

--- src/static/site/js/changeyear.js
+++ src/static/site/js/changeyear.js
@@ -1,5 +1,6 @@
 django.jQuery( function() {
     django.jQuery(".hasDatepicker").datepicker('option', 'changeYear', true);
+    django.jQuery(".hasDatepicker").datepicker('option', 'yearRange', '-100:+0');
     var language_code = django.jQuery('html').attr('lang');
     django.jQuery(".hasDatepicker").datepicker('option', django.jQuery.datepicker.regional[language_code]);
 });
