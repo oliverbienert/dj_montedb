242a5a2bbf7b363cab3d76e8df32fe6de7562f71
Adrian Bielefeldt <martenbochum@gmx.de>
2020-04-14 02:11:43 +0200

    Added temporary fee site for development

--- src/montedb/urls.py
+++ src/montedb/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import AdultsView, ChildrenView
+from .views import AdultsView, ChildrenView, ParentFeeView
 
 app_name = 'montedb'
 
 urlpatterns = [
     path('children/', ChildrenView.as_view(), name='child-list'),
     path('adults/', AdultsView.as_view(), name='adult-list'),
+    path('fee/<int:pk>/', ParentFeeView.as_view(), name='parent-fee')
 ]
