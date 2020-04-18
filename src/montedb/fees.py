84a954d204b849f7911f1e0a317060d9b632f7b8
Adrian Bielefeldt <martenbochum@gmx.de>
2020-04-18 23:24:07 +0200

    Added adult calculations to fee

--- src/montedb/fees.py
+++ src/montedb/fees.py
@@ -9,6 +9,31 @@ class ChildCalculation:
     fee = None
 
 
+class AdultCalculation:
+    salary = None
+    income = None
+    unemployment = None
+    other_payments = None
+    deduction = None
+    total_income = None
+
+    def set_income(self, income):
+        income_type = income.type
+        income_amount = income.amount
+        if income_type == Income.SALARY:
+            self.salary = income_amount
+        elif income_type == Income.INCOME:
+            self.income = income_amount
+        elif income_type == Income.UNEMPLOYMENT:
+            self.unemployment = income_amount
+        elif income_type == Income.OTHER_PAYMENTS:
+            self.other_payments = income_amount
+        elif income_type == Income.DEDUCTION:
+            self.deduction = -income_amount
+        else:
+            print("WARNING: Income type {} is not known for AdultCalculation!".format(income_type))
+
+
 def get_adults(adult):
     adults = set()
     for child in adult.children.all():
@@ -29,21 +54,37 @@ class Fee:
     reduction2 = 0
     adults = set()
     children = set()
+    payer = None
+    adult_calculations = dict()
+    children_calculations = dict()
 
-    def calc(self, adult):
+    def __init__(self, adult):
         self.adults = get_adults(adult)
         self.children = get_children(adult)
+
         self.calc_total_income()
         self.calc_reduction2()
-        return self.calc_fee()
+        self.calc_fee()
 
     def calc_total_income(self):
         for adult in self.adults:
+            # TODO Should be choosen based on payer flag as soon as it is introduced
+            self.payer = adult
+            adult_calculation = AdultCalculation()
+            adult_total_income = 0
+
             for income in adult.income.all():
+                income_amount = income.amount
                 if income.type == Income.DEDUCTION:
-                    self.total_income -= income.amount
+                    self.total_income -= income_amount
+                    adult_total_income -= income_amount
                 else:
                     self.total_income += income.amount
+                    adult_total_income += income_amount
+                adult_calculation.set_income(income)
+
+            adult_calculation.total_income = adult_total_income
+            self.adult_calculations[adult] = adult_calculation
 
     def calc_reduction2(self):
         total = len(self.adults) + len(self.children)
@@ -60,7 +101,6 @@ class Fee:
 
     def calc_fee(self):
         reduction1 = 0
-        result = dict()
         for child in sorted(self.children, key=lambda x: x.birth_date):
             child_calculation = ChildCalculation()
             child_calculation.income = self.total_income
@@ -82,6 +122,4 @@ class Fee:
             child_calculation.incomeApplied = income
             child_calculation.fee = contribution.contribution
 
-            result[child] = child_calculation
-
-        return result
+            self.children_calculations[child] = child_calculation
