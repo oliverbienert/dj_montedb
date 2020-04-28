ae3d82d2764439339a67dadbd72e43167698e483
Oliver (T560) <olli.bienert@gmail.com>
2020-04-28 22:44:15 +0200

    Added django_extensions

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -77,6 +77,7 @@ INSTALLED_APPS = (
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'django_extensions',
     "authtools",
     "crispy_forms",
     "django_tables2",
