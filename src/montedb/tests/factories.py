86aae818a69dc38a37de8b7504be4e73680a01b1
Adrian Bielefeldt <martenbochum@gmx.de>
2020-06-16 18:21:31 +0200

    Added basic person test

--- src/montedb/tests/factories.py
+++ src/montedb/tests/factories.py
@@ -1,7 +1,12 @@
 import factory
-from ..models import Address
+from ..models import Address, Person
 
 
 class AddressFactory(factory.DjangoModelFactory):
     class Meta:
         model = Address
+
+
+class PersonFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = Person
