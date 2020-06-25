f5177dce350d6d70615a5b50e51db01e16e2a807
Oliver (T560) <olli.bienert@gmail.com>
2020-06-25 22:15:46 +0200

    Added conftest.py

---
+++ conftest.py
@@ -0,0 +1,15 @@
+import django
+from django.conf import settings
+
+
+# `pytest` automatically calls this function once when tests are run.
+def pytest_configure():
+    settings.DEBUG = False
+    # If you have any test specific settings, you can declare them here,
+    # e.g.
+    # settings.PASSWORD_HASHERS = (
+    #     'django.contrib.auth.hashers.MD5PasswordHasher',
+    # )
+    django.setup()
+    # Note: In Django =< 1.6 you'll need to run this instead
+    # settings.configure()
