cb9750156bcbb183c519a71c4a6db29fd294e7a4
Oliver (T560) <olli.bienert@gmail.com>
2020-07-01 00:30:44 +0200

    Fixed test_child test

--- src/config/tests/factories.py
+++ src/config/tests/factories.py
@@ -1,5 +1,7 @@
+from datetime import date
+
 import factory
-from montedb.models import Address, Person
+from montedb.models import Address, Person, Child
 
 
 class AddressFactory(factory.DjangoModelFactory):
@@ -10,3 +12,15 @@ class AddressFactory(factory.DjangoModelFactory):
 class PersonFactory(factory.DjangoModelFactory):
     class Meta:
         model = Person
+
+
+class ChildFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = Child
+
+    last_name = 'last name'
+    first_name = 'first name'
+    birth_date = date(2010, 1, 1)
+    birth_place = 'place'
+    care_time = 2
+    kita = False
