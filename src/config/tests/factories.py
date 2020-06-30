7e459f748b81d3cf50c4b9440d2f02f8bb643216
Oliver (T560) <olli.bienert@gmail.com>
2020-06-30 22:28:41 +0200

    Added default values to factory classes

--- src/config/tests/factories.py
+++ src/config/tests/factories.py
@@ -1,3 +1,4 @@
+import datetime
 import factory
 from montedb.models import Address, Person
 
@@ -6,7 +7,16 @@ class AddressFactory(factory.DjangoModelFactory):
     class Meta:
         model = Address
 
+    street = 'Street'
+    house_number = '1'
+    zip_code = '12345'
+    city = 'City'
+
 
 class PersonFactory(factory.DjangoModelFactory):
     class Meta:
         model = Person
+
+    last_name = 'last name'
+    first_name = 'first name'
+    birth_date = datetime.date(2020, 1, 1)
